4f2d0c9da72765ef4f806367f44db7a384f079a9
Amina <aminashajanat@gmail.com>
2025-10-23 12:29:14 +0530

    16/10/2025--sql

---
+++ sql.sql
@@ -0,0 +1,5 @@
+create database db;
+insert into student values(201,"Amina","Tamilnadu",20,90,"A1");
+select * from student;
+insert into student values(202,"Arif","Tamilnadu",16,91,"A1"),(203,"kendel","California",29,99,"A1");
+select * from student;
